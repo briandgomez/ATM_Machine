2f1ea52e332ca9916f12102f5a74cc5860216c7b
Brian Gomez <briandgomez98@gmail.com>
2023-09-16 00:11:27 -0500

    added code to start and connect to mysql server

--- src/App.java
+++ src/App.java
@@ -1,6 +1,30 @@
+import java.sql.Connection;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        MainMenu options = new MainMenu();
-        options.Menu();
+        try {
+             // Connection to db
+            Connection connection = DatabaseConnector.getConnection();
+            System.out.println("Successfully connected to MySQL server!");
+
+            // Create database
+            DatabaseConnector.createDatabase();
+
+            // Create table
+            DatabaseConnector.createTable();
+            
+            // Insert values into exsiting table
+            DatabaseOperations databaseOperations = new DatabaseOperations(connection);
+            databaseOperations.performDatabaseOperations();
+
+            // Perform transactions
+            MainMenu options = new MainMenu();
+            options.Menu(connection);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DatabaseConnector.closeConnection();
+        }
     }
 }
