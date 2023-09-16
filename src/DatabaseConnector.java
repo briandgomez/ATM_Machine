b629a3dfb086e36750efa702c2b0f3ea53288738
Brian Gomez <briandgomez98@gmail.com>
2023-09-16 00:15:40 -0500

    create db and table and connect to db

---
+++ src/DatabaseConnector.java
@@ -0,0 +1,69 @@
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+    // Start db server
+    private static final String jdbcUrl = "jdbc:mysql://localhost:3306/atm_machine_sample_db";
+    private static final String username = "root";
+    private static final String password = "tannebaum55$";
+    private static final String databasename = "atm_machine_sample_db";
+
+    private static Connection connection;
+
+    public static void createTable() {
+        try {
+            Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
+            Statement statement = conn.createStatement();
+            statement.executeUpdate("DROP TABLE IF EXISTS " + databasename);
+
+            String sql = "CREATE TABLE atm_machine_sample_table " +
+                    "(id INTEGER NOT NULL AUTO_INCREMENT, "
+                    + "first VARCHAR(255), "
+                    + "last VARCHAR(255), "
+                    + "age INTEGER, "
+                    + "PRIMARY KEY (id))";
+
+            statement.executeUpdate(sql);
+            System.out.println("Table created");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void createDatabase() {
+        try {
+            Connection conn = getConnection();
+            Statement statement = conn.createStatement();
+            statement.executeUpdate("DROP TABLE IF EXISTS atm_machine_sample_table");
+
+            System.out.println("Database successfully created!");
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                // Connect to db using credentials
+                connection = DriverManager.getConnection(jdbcUrl, username, password);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
