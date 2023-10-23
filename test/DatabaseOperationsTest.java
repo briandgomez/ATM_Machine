d4abd328648c0a6573b47f204aa96523f259536d
Brian Gomez <briandgomez98@gmail.com>
2023-10-22 20:22:50 -0500

    created testGetCurrentBalanceForValidCardNumber unit test

--- test/DatabaseOperationsTest.java
+++ test/DatabaseOperationsTest.java
@@ -1,22 +1,32 @@
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class DatabaseOperationsTest {
     private static DatabaseOperations dbOperations;
     private static Connection mockConnection;
     private static PreparedStatement mockPreparedStatement;
     private static PreparedStatement mockPreparedStatement2;
+    private static ResultSet mockResultSet;
 
-    @BeforeClass
-    public static void setUp() throws SQLException {
+    @Before
+    public void setUp() throws SQLException {
         // simulate connection
         mockConnection = mock(Connection.class);
         // simulate preparedstatements
@@ -41,10 +51,34 @@ public class DatabaseOperationsTest {
         verify(mockConnection).prepareStatement(
                 "INSERT INTO atm_machine_sample_table_2 (primary_account_number, pin, balance, date) VALUES (?, ?, ?, ?)");
 
-
         // check that the insertion to db statements were called once
         verify(mockPreparedStatement, times(1)).executeUpdate();
         verify(mockPreparedStatement2, times(1)).executeUpdate();
 
+    }
+
+    @Test
+    public void testGetCurrentBalanceForValidCardNumber() throws SQLException {
+        long validCardNumber = 1234567891;
+        long expectedBalance = 9500L;
+
+        ResultSet mockResultSet = mock(ResultSet.class);
+
+        when(mockPreparedStatement.executeQuery()).thenReturn(mockResultSet);
+        when(mockResultSet.next()).thenReturn(true);
+        when(mockResultSet.getLong("balance")).thenReturn((expectedBalance));
+
+        long actualBalance = dbOperations.getCurrentBalance(validCardNumber);
+        System.out.println(actualBalance);
+        String expectedSql = "SELECT balance FROM atm_machine_sample_table WHERE primary_account_number = ?";
+        verify(mockConnection).prepareStatement(expectedSql);
+        verify(mockPreparedStatement).setLong(1, validCardNumber);
+
+        verify(mockPreparedStatement).executeQuery();
+
+        verify(mockResultSet).next();
+        verify(mockResultSet).getLong("balance");
+
+        assertEquals(expectedBalance, actualBalance);
     }
 }
